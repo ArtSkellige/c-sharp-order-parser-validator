7effb545ad8bdc828c35e188590e132edc1ddaf4
ArtSkellige <ArtSkelligeBravoZulu141@protonmail.com>
2026-01-31 04:14:28 +0300

    Improve variable naming: order -> orderID for clarity

--- Program.cs
+++ Program.cs
@@ -9,14 +9,14 @@ string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
 string[] orderIDs = orderStream.Split(",");
 Array.Sort(orderIDs);
 
-foreach (string order in orderIDs)
+foreach (string orderID in orderIDs)
 {
-    if (order.Length != 4)
+    if (orderID.Length != 4)
     {
-        Console.WriteLine($"{order} - Error!");
+        Console.WriteLine($"{orderID} - Error!");
     }
     else
     {
-        Console.WriteLine($"{order}");
+        Console.WriteLine($"{orderID}");
     }
 }
